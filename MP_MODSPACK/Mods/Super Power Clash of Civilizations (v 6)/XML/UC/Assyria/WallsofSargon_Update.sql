3942bf3a9c690ecf5f390ef1e60ec9fc419acfb4
Karan Rawat <maxcore1912@gmail.com>
2022-01-18 15:27:50 +0530

    1.4 Prep

--- MP_MODSPACK/Mods/Super Power Clash of Civilizations (v 6)/XML/UC/Assyria/WallsofSargon_Update.sql
+++
@@ -1,8 +0,0 @@
---Increases boost of Walls of Sargon, if 3UC is installed.
-
-------------------------------
--- Building_YieldChanges
-------------------------------
-UPDATE Building_YieldChanges
-SET	Yield=2
-WHERE BuildingType='BUILDING_SARGON3_FORT' AND YieldType='YIELD_PRODUCTION';
