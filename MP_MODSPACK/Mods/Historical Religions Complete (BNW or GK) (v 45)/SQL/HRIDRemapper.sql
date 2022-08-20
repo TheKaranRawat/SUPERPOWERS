2bb21e6e3cc6da1c2ed135ec4095870ba3e5b75d
Karan Rawat <37260572+TheKaranRawat@users.noreply.github.com>
2022-08-20 13:51:30 +0530

    1.5.7 Upload

--- MP_MODSPACK/Mods/Historical Religions Complete (BNW or GK) (v 45)/SQL/HRIDRemapper.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE HRIDRemapper ( id INTEGER PRIMARY KEY AUTOINCREMENT, Type TEXT );
-INSERT INTO HRIDRemapper (Type) SELECT Type FROM Religions ORDER BY ID;
-UPDATE Religions SET ID =	( SELECT HRIDRemapper.id-1 FROM HRIDRemapper WHERE Religions.Type = HRIDRemapper.Type);
-DROP TABLE HRIDRemapper;
